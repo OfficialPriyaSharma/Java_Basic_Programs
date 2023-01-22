2f7ad9347f9b513852e0ebfb8ec3e6e60add1cbe
Priya <sharma27598@gmail.com>
2023-01-22 19:22:07 +0530

    Initial commit

---
+++ src/HashMapExample.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+
+        public static void main(String[] args) {
+
+            HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+            map.put("One", 1);
+            map.put("Two", 2);
+            map.put("Three", 3);
+            map.put("Four", 4);
+            map.put("Five", 5);
+
+            int value = map.get("Two");
+            System.out.println(value);
+        }
+    }
+
